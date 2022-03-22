ce0c636c74a0b7ac1ed32b1d1e88944da6d63af5
Pagwin <git@pagwin.xyz>
2022-03-22 13:02:14 -0400

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,103 @@
-fn main() {
-    println!("Hello, world!");
+extern crate rust_sc2;
+
+
+use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
+use rust_sc2::{prelude::*, units::*};
+use std::{iter::Sum, collections::HashSet};
+
+type SRes = SC2Result<()>;
+
+const OVIE_BUILD:HashSet<AbilityId> = {
+    let mut t = HashSet::new();
+    t.insert(AbilityId::LarvaTrainOverlord);
+    t
+};
+
+#[bot]
+#[derive(Default)]
+struct Hassium{
+    latest_hatch:Option<Unit>,
+    latest_sat: u8
+}
+impl Hassium{
+    fn init(mut self) -> Self {
+        self.latest_hatch = Some(self.units.my.townhalls.first().unwrap().clone());
+        self.latest_sat = 12;
+        self
+    }
+    fn next_hatch_loc(units:&PlayerUnits, expands: Vec<Point2>) -> Point2{
+
+        let taken = units.townhalls.iter()
+            .chain(units.placeholders.iter())
+            .map(Unit::position)
+            .collect::<std::collections::HashSet<Point2>>();
+
+        let taken_midpoint:Point2 = Sum::<Point2>::sum(taken.iter().map(|v|v.clone()));
+        //this is dumb
+        let taken_midpoint = taken_midpoint / taken.len() as f32;
+
+        let available = expands.iter()
+            .filter(|v|!taken.contains(v));
+        let dist = |point:&Point2| taken_midpoint.distance(point);
+        available.min_by(|point1,point2|dist(point1).partial_cmp(&dist(point2)).unwrap_or(std::cmp::Ordering::Equal)).unwrap().clone()
+    }
+    fn currently_ordering(units:&Units, ids:HashSet<AbilityId>) -> HashSet<AbilityId>{
+        //the fact that this has to be so complicated is dumb
+        ids.iter()
+            .filter(|a| units.iter()
+                .any(|u|u.orders().iter()
+                    .any(|o|o.ability == **a)
+                    ))
+            .map(AbilityId::clone)
+            .collect::<HashSet<AbilityId>>()
+    }
+}
+impl Player for Hassium{
+    fn get_player_settings(&self) -> PlayerSettings {
+        PlayerSettings::new(Race::Zerg)
+            .with_name("Hassium")
+            .raw_crop_to_playable_area(true)
+            .raw_affects_selection(false)
+    }
+    fn on_step(&mut self, iteration: usize) -> SRes {
+
+        while self.latest_sat < 16 {
+            if self.can_afford(UnitTypeId::Drone, true) {
+                self.units.my.larvas.pop().unwrap().train(UnitTypeId::Drone,false);
+            }
+            else if self.supply_left < 1 {
+                //should probably split off this condition into a method
+                if Hassium::currently_ordering(&self.units.my.units, OVIE_BUILD).is_empty() {
+                    self.units.my.larvas.pop().unwrap().train(UnitTypeId::Overlord, false);
+                }
+            }
+        }
+        
+        if self.minerals >= 350 && !self.units.my.larvas.is_empty() && self.latest_sat == 16 {
+
+            let dist = |point:Point2| self.latest_hatch.clone().unwrap()
+                .position().distance(point);
+
+            let cmp = |f:&f32,s:&f32|f32::partial_cmp(f,s).unwrap();
+
+            self.units.my.workers.iter()
+                .map(Unit::position)
+                .map(dist)
+                .min_by(cmp)
+                .unwrap();
+            
+        }
+
+        Ok(())
+    }
+}
+
+fn main() -> SRes {
+    run_vs_human(&mut Hassium::default().init(), 
+        PlayerSettings::new(Race::Zerg)
+            .with_name("pagwin")
+            .raw_crop_to_playable_area(false)
+            .raw_affects_selection(true),
+        "EternalEmpireLE",
+        LaunchOptions::default())
 }
